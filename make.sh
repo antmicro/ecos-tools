6e34ac11002ceb9e2b2664558bbbb101a20618b4
Michael Gielda <mgielda@antmicro.com>
2013-09-12 16:24:15 +0200

    Changes to how the system works, closes #1

--- make.sh
+++ make.sh
@@ -1,43 +1,92 @@
 #!/bin/sh
 
-# TODO: let application reside in other directories
-# TODO: include flags to only compile kernel etc.
+TOOLS_PATH=$(dirname $(readlink -f $0))
 
-if [ -z $1 ]
+function get_option {
+    VALUE=`echo $1 | sed 's/[-a-zA-Z0-9]*=//'`
+}
+
+function usage {
+        echo "Usage: `basename $0` --config=<config-name> (--kernel|--output-filename=<filename>|--tests|--rebuild)"
+}
+
+TESTS=false
+REBUILD=false
+
+for i in "$@"
+do
+get_option $i
+case $i in
+    -c=*|--config=*)
+        CONFIG=$VALUE
+        ;;
+    -o=*|--output-filename=*)
+        OUTPUT_FILENAME=$VALUE    
+        ;;
+    -t|--tests)
+        TESTS=true
+        ;;
+    -r|--rebuild)
+        REBUILD=true
+	;;
+    *)
+        echo "FATAL: You have provided an unknown option: $i."
+	usage
+        exit 1
+esac
+done
+
+if [ -z $CONFIG ]
 then
-	echo "Usage: `basename $0` <config-name>"
+	echo "FATAL: No config file provided."
+	usage
 	exit 1
 fi
 
-CONFIG_FILE=config.$1
+CONFIG_FILE=$CONFIG.config
 
 if [ ! -e $CONFIG_FILE ]
 then
-	echo "The config file \"$CONFIG_FILE\" does not exist."
+	echo "FATAL: The config file \"$CONFIG_FILE\" does not exist."
 	exit 1
 fi
 
 # include config file to set appropriate variables
 . ./$CONFIG_FILE
 
-if [ -z ECOS_REPOSITORY ] || [ -z ECC ]
+if [ -z $ECOS_REPOSITORY ] || [ -z $ECC ]
 then
-	echo "The variables \"ECOS_REPOSITORY\" AND \"ECC\" have not been set properly."
+	echo "FATAL: The variables \"ECOS_REPOSITORY\" AND \"ECC\" have not been set properly."
 	exit 1
 fi
 
-mkdir -p $1-build
-rm -rf $1-build/*
-cd $1-build
+ECOS_REPOSITORY=`readlink -f $ECOS_REPOSITORY`
+ECC=`readlink -f $ECC`
+
+mkdir -p $CONFIG\_build
+if $REBUILD ; then
+  rm -rf $CONFIG\_build/*
+fi
+cd $CONFIG\_build
 KPATH=`pwd`
 
-../ecosconfig --config=../$ECC tree
-make
+$TOOLS_PATH/ecosconfig --config=$ECC tree
+if $TESTS ; then
+   make tests
+   exit
+else
+   make
+fi
 
 cd ..
 
-OPT="-std=gnu99 -Wpointer-arith -Winline -Wundef -g -nostdlib -ffunction-sections -fdata-sections -fno-exceptions"
-
-# compile the application
-arm-none-eabi-gcc -g -I./ -g -I${KPATH}/install/include ${FILES} \
-	-L${KPATH}/install/lib -Ttarget.ld ${ARCH_OPT} ${OPT} ${ADD_OPT} -o $1 
+if [ ! -z $FILES ]
+then
+   if [ -z $OUTPUT_FILENAME ] ; then
+      OUTPUT_FILENAME=$CONFIG
+   fi
+   # compile the application and link it against kernel
+   OPT="-std=gnu99 -Wpointer-arith -Winline -Wundef -g -nostdlib -ffunction-sections -fdata-sections -fno-exceptions"
+   arm-none-eabi-gcc -g -I./ -g -I${KPATH}/install/include ${FILES} \
+   	-L${KPATH}/install/lib -Ttarget.ld ${ARCH_OPT} ${OPT} ${ADD_OPT} -o $OUTPUT_FILENAME
+fi
