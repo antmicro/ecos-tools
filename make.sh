987c2d0165eec236b23d6f2d1fb584f3ba574a2b
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2013-12-11 21:48:11 +0100

    Quote all variables to avoid issues with spaces.

--- make.sh
+++ make.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-TOOLS_PATH="$(dirname $(readlink -f $0))"
+TOOLS_PATH="$(dirname $(readlink -f "$0"))"
 
 function get_option {
-    VALUE="$(echo $1 | sed 's/[-a-zA-Z0-9]*=//')"
+    VALUE="$(echo "$1" | sed 's/[-a-zA-Z0-9]*=//')"
 }
 
 function ecc_get_value {
-    echo $(grep $1 $ECC -A 100 | grep user_value | head -1 |\
+    echo $(grep "$1" "$ECC" -A 100 | grep user_value | head -1 |\
         sed 's,user_value,|,g' | cut -f 2 -d '|' | tr -d '"')
 }
 
 function usage {
-    echo "Usage: $(basename $0) --config=<config-name> (--output-filename=<filename>|--tests|--rebuild)"
+    echo "Usage: $(basename "$0") --config=<config-name> (--output-filename=<filename>|--tests|--rebuild)"
     echo "       Use a config with FILES='' to just generate a kernel"
 }
 
 for i in "$@"
 do
-    get_option $i
-    case $i in
+    get_option "$i"
+    case "$i" in
         -c=*|--config=*)
-            CONFIG=$VALUE
+            CONFIG="$VALUE"
             ;;
         -o=*|--output-filename=*)
-            OUTPUT_FILENAME=$VALUE    
+            OUTPUT_FILENAME="$VALUE"
             ;;
         -t|--tests)
             TESTS=true
@@ -39,44 +39,44 @@ do
     esac
 done
 
-if [[ -z $CONFIG ]]
+if [[ -z "$CONFIG" ]]
 then
     echo "FATAL: No config file provided."
     usage
     exit 1
 fi
 
-CONFIG_FILE=$CONFIG.config
+CONFIG_FILE="$CONFIG.config"
 
-if [[ ! -e $CONFIG_FILE ]]
+if [[ ! -e "$CONFIG_FILE" ]]
 then
     echo "FATAL: The config file \"$CONFIG_FILE\" does not exist."
     exit 1
 fi
 
 # include config file to set appropriate variables
-. ./$CONFIG_FILE
+. ./"$CONFIG_FILE"
 
-if [[ -z $ECOS_REPOSITORY ]] || [[ -z $ECC ]]
+if [[ -z "$ECOS_REPOSITORY" ]] || [[ -z "$ECC" ]]
 then
     echo "FATAL: The variables \"ECOS_REPOSITORY\" AND \"ECC\" have not been set properly."
     exit 1
 fi
 
-ECOS_REPOSITORY=$(readlink -f $ECOS_REPOSITORY)
-ECC=$(readlink -f $ECC)
+ECOS_REPOSITORY="$(readlink -f "$ECOS_REPOSITORY")"
+ECC="$(readlink -f "$ECC")"
 
 GCC=$(ecc_get_value CYGBLD_GLOBAL_COMMAND_PREFIX)-gcc
 CFLAGS=$(ecc_get_value CYGBLD_GLOBAL_CFLAGS)
 
-mkdir -p $CONFIG\_build
+mkdir -p "$CONFIG"\_build
 if "${REBUILD:-false}"; then
-    rm -rf $CONFIG\_build/*
+    rm -rf "$CONFIG"\_build/*
 fi
-cd $CONFIG\_build
-KPATH=$(pwd)
+cd "$CONFIG"\_build
+KPATH="$(pwd)"
 
-$TOOLS_PATH/ecosconfig --config=$ECC tree
+"$TOOLS_PATH"/ecosconfig --config="$ECC" tree
 if "${TESTS:-false}"; then
     make tests
     exit
@@ -86,14 +86,14 @@ fi
 
 cd ..
 
-if [[ ! -z $FILES ]]
+if [[ ! -z "$FILES" ]]
 then
-    if [[ -z $OUTPUT_FILENAME ]]; then
-        OUTPUT_FILENAME=$CONFIG
+    if [[ -z "$OUTPUT_FILENAME" ]]; then
+        OUTPUT_FILENAME="$CONFIG"
     fi
 
     echo "Compiling eCos application..."
-    PATH="${TOOLS_PATH}:$PATH" $GCC -g -I./ -g -I${KPATH}/install/include \
-        ${FILES} -L${KPATH}/install/lib -Ttarget.ld ${CFLAGS} ${ADD_OPT} \
-        -nostdlib -o $OUTPUT_FILENAME
+    PATH="${TOOLS_PATH}:$PATH" "$GCC" -g -I./ -g -I"${KPATH}"/install/include \
+        "${FILES}" -L"${KPATH}"/install/lib -Ttarget.ld "${CFLAGS}" "${ADD_OPT}" \
+        -nostdlib -o "$OUTPUT_FILENAME"
 fi
